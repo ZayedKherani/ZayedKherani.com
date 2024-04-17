655207d5fdab252788ca3e4705d0f996d0dc716d
Zayed Kherani <Zayedkherani@gmail.com>
2024-04-17 03:04:13 -0400

    https://mailchannel.zayedkherani.workers.dev/

--- js/mailchannels-cloudflare-worker.js
+++ js/mailchannels-cloudflare-worker.js
@@ -1,49 +1,3 @@
-// // mailchannels-worker.js
-// document.addEventListener("DOMContentLoaded", function () {
-//   const form = document.getElementById("contact-form");
-
-//   form.addEventListener("submit", function (event) {
-//     event.preventDefault(); // Prevent the default form submission
-
-//     // Gather the data from the form
-//     const name = document.getElementById("name").value;
-//     const email = document.getElementById("email").value;
-//     const subject = document.getElementById("subject").value;
-//     const message = document.getElementById("message").value;
-
-//     // Create the request payload
-//     const payload = {
-//       email: email,
-//       name: name,
-//       subject: subject,
-//       message: message
-//     };
-
-//     // Send the data to the MailChannels worker
-//     fetch("https://mainchannelworker.zayedkherani.com/", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(payload)
-//     })
-//       .then((response) => {
-//         if (response.ok) {
-//           // Display a success message
-//           alert("Message sent successfully!");
-//           // Clear the form
-//           form.reset();
-//         } else {
-//           // Handle errors
-//           alert("Failed to send message. Please try again later.");
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("There was an error sending the message:", error);
-//         alert("An error occurred. Please try again later.");
-//       });
-//   });
-// });
-
-// mailchannels-worker.js
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contact-form");
 
@@ -65,12 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     // Send the data to the MailChannels worker
-    fetch("https://mainchannelworker.zayedkherani.com/", {
+    fetch("https://mailchannel.zayedkherani.workers.dev/", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
-      body: payload
+      body: JSON.stringify(payload)
     })
       .then((response) => {
         if (response.ok) {
